567c9f04196ddd16c907a438f40a7e3ea3a9828a
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-11-14 12:50:36 +0100

    Looking for error with paths

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,72 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact
+} = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
+const Joi = require("joi");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required()
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", async (req, res, next) => {
+  try {
+    const contact = await getContactById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const { error } = contactSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { name, email, phone } = req.body;
+  const newContact = await addContact({ name, email, phone });
+  res.status(201).json(newContact);
+});
 
-module.exports = router
+router.delete("/:id", async (req, res, next) => {
+  const removedContact = await removeContact(req.params.id);
+  if (!removedContact) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  res.status(200).json({ message: "contact deleted" });
+});
+
+router.put("/:id", async (req, res, next) => {
+  const { error } = contactSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const updatedContact = await updateContact(req.params.id, req.body);
+  if (!updatedContact) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  res.status(200).json(updatedContact);
+});
+
+module.exports = router;
