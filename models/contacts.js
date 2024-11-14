cb4cfc16766bd28cde068095769fd1fd242a2d34
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-11-14 15:19:09 +0100

    Ready to check

--- models/contacts.js
+++ models/contacts.js
@@ -7,7 +7,7 @@ const contactsPath = path.join(__dirname, "./contacts.json");
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath, "utf8");
   return JSON.parse(data);
-};
+}; 
 
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
