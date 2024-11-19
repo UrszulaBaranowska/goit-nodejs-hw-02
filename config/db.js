db2fea06a1c4dc319ec74f527a81727f9d89750c
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-11-19 12:57:09 +0100

    db.js upgrade

--- config/db.js
+++ config/db.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-// const username = encodeURIComponent("uwilinska");
-// const password = encodeURIComponent("8V3wYECxKCfYflSQ");
-// const cluster = "cluster0.cwimq.mongodb.net";
+const username = encodeURIComponent("uwilinska");
+const password = encodeURIComponent("8V3wYECxKCfYflSQ");
+const cluster = "cluster0.cwimq.mongodb.net";
 const dbName = "db-contacts";
 
-const uri = `mongodb+srv://uwilinska:8V3wYECxKCfYflSQ@cluster0.cwimq.mongodb.net/db-contacts?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
 
 async function connectDB() {
   try {
-    await mongoose.connect(uri, { dbName });
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log("Database connection successful");
   } catch (error) {
     console.error("Database connection error:", error.message);
