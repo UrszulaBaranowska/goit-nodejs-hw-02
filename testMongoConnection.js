df27943c63c66a1704cc0b54d94d72b60bdfc50c
Urszula Baranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-11-25 22:07:04 +0100

    Update testMongoConnection.js

--- testMongoConnection.js
+++ testMongoConnection.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-// const username = encodeURIComponent("uwilinska");
-// const password = encodeURIComponent("8V3wYECxKCfYflSQ");
-// const cluster = "cluster0.cwimq.mongodb.net";
-// const dbName = "db-contacts";
+
 
 const uri = `mongodb+srv://uwilinska:8V3wYECxKCfYflSQ@cluster0.cwimq.mongodb.net/db-contacts?retryWrites=true&w=majority`;
 
